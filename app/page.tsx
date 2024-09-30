98528fb408ffb277f02c1727d67975829b997b37
abhannouni <abdeslam.hannouni@gmail.com>
2024-09-30 07:05:29 +0100

    improve layout and card structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,81 @@
-import Image from "next/image";
+// import BarChart from './components/BarChartUsageExampleWithClickEvent';
+import { BankAccountsCard, ProfitLossCard, ExpensesCard, SalesCard, SetupChecklistCard, AccountsReceivableCard, CashFlowTrendCard } from './components/Card';
 
-export default function Home() {
+export default function Page() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="p-6 w-full ">
+      {/* Header */}
+      <div className="flex justify-between items-center mb-6">
+        <h1 className="text-2xl font-bold">Home</h1>
+        <button className="bg-orange-500 text-white px-4 py-2 rounded-md">Customise layout</button>
+      </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      {/* Main content */}
+      <div className="flex flex-col gap-6">
+        {/* Top row */}
+        <div className="flex gap-6">
+          {/* Setup checklist */}
+          <div className="w-6/12 bg-white p-6 rounded-lg shadow-md">
+            <SetupChecklistCard />
+          </div>
+          <div className="w-6/12">
+            {/* Right Column */}
+            <div className="flex gap-6">
+              {/* Bank accounts */}
+              <div className="mb-6 w-1/2">
+                <BankAccountsCard />
+              </div>
+              <div className="mb-6 w-1/2">
+                <ProfitLossCard />
+              </div>
+            </div>
+
+            {/* Right Column for Expenses and Sales  */}
+            <div className="flex gap-6">
+              <div className="mb-6 w-1/2">
+                <ExpensesCard />
+              </div>
+              <div className="mb-6 w-1/2">
+                <SalesCard />
+              </div>
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        {/* Middle row */}
+        <div className="flex gap-6">
+          {/* Cash flow trend */}
+          <div className="w-6/12">
+            {/* <BarChart /> */}
+            <CashFlowTrendCard />
+          </div>
+
+          {/* Tasks */}
+          <div className="w-6/12 bg-white p-4 rounded-lg shadow">
+            <h2 className="font-bold mb-4">TASKS</h2>
+            <p>No tasks yet!</p>
+          </div>
+        </div>
+
+        {/* Bottom row */}
+        <div className="flex gap-6">
+          {/* Tasks */}
+          <div className="w-6/12 bg-white p-4 rounded-lg shadow">
+            <h2 className="font-bold mb-4">TASKS</h2>
+            <p>No tasks yet!</p>
+          </div>
+          <div className="w-6/12 flex gap-6">
+
+            {/* Accounts Receivable and Payable */}
+            <div className="w-1/2">
+              <AccountsReceivableCard />
+            </div>
+            <div className="w-1/2">
+              <AccountsReceivableCard />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
